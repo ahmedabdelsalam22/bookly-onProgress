a580bd3c9dac7c68848cdb2ab79a4a6c4609184f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-21 00:06:36 +0200

    add animation to splash_screen_body.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         scaffoldBackgroundColor: ColorConstants.kPrimaryColor,
         primarySwatch: Colors.blue,
