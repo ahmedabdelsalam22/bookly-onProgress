e1e95bbc569a1a15ce46546e3620978416d2ee8a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-20 22:53:44 +0200

    splash_screen_body.dart

---
+++ lib/presentation_layer/view/splash_view/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/presentation_layer/view/splash_view/splash_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
