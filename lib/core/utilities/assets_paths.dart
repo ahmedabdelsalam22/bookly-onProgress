7d24ff9d43e1b8daec76273f8e268c3b5c42b20a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-21 13:45:19 +0200

    add book_list_view to home_view_body.dart

--- lib/core/utilities/assets_paths.dart
+++ lib/core/utilities/assets_paths.dart
@@ -1,3 +1,4 @@
 class AssetPath {
   static const String logoImagePath = "assets/images/Logo.png";
+  static const String bookImage = "assets/images/test_image.png";
 }
---
+++ lib/presentation_layer/widgets/books_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_book_item.dart';
+
+class BooksListView extends StatelessWidget {
+  const BooksListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: CustomBookItem(),
+          );
+        },
+        itemCount: 5,
+      ),
+    );
+  }
+}
