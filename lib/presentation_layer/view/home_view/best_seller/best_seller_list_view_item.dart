ce7c0155a2832c5f72967ba621de8bdfbbdbb55e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-03 13:41:28 +0200

    add book_details_view.dart and navigation with getx package

--- lib/presentation_layer/view/home_view/best_seller/best_seller_list_view_item.dart
+++ lib/presentation_layer/view/home_view/best_seller/best_seller_list_view_item.dart
@@ -1,90 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 import '../../../../core/utilities/assets_paths.dart';
 import '../../../../core/utilities/text_styles.dart';
+import '../../book_details_view/book_details_view.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.7 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.red,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetPath.bookImage,
+    return InkWell(
+      onTap: () {
+        Get.to(const BookDetailsView());
+      },
+      child: SizedBox(
+        height: 150,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetPath.bookImage,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Text(
-                  'Harry Potter \n and The Goblet of Fire',
-                  style:
-                      AppTextStyles.textStyle20.copyWith(fontFamily: 'sectra'),
+            const SizedBox(
+              width: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry Potter \n and The Goblet of Fire',
+                    style: AppTextStyles.textStyle20
+                        .copyWith(fontFamily: 'sectra'),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+                const SizedBox(
+                  height: 7,
+                ),
+                Text(
+                  'J.K Rowing',
+                  style: AppTextStyles.textStyle14
+                      .copyWith(color: Colors.grey[150]),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
-              ),
-              const SizedBox(
-                height: 7,
-              ),
-              Text(
-                'J.K Rowing',
-                style:
-                    AppTextStyles.textStyle14.copyWith(color: Colors.grey[150]),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "19.09\$",
-                    style: AppTextStyles.textStyle20.copyWith(
-                        fontFamily: 'pro', fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.1,
-                  ),
-                  Row(
-                    children: const [
-                      Icon(
-                        FontAwesomeIcons.solidStar,
-                        color: Colors.amberAccent,
-                      ),
-                      SizedBox(width: 3),
-                      Text(
-                        '4.9  ${'(2150)'}',
-                        style: AppTextStyles.textStyle16,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "19.09\$",
+                      style: AppTextStyles.textStyle20.copyWith(
+                          fontFamily: 'pro', fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.solidStar,
+                          color: Colors.amberAccent,
+                        ),
+                        SizedBox(width: 3),
+                        Text(
+                          '4.9  ${'(2150)'}',
+                          style: AppTextStyles.textStyle16,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/presentation_layer/widgets/custom_book_item.dart
+++ lib/presentation_layer/widgets/custom_book_item.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../core/utilities/assets_paths.dart';
+import '../view/book_details_view/book_details_view.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image: const DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(
-              AssetPath.bookImage,
+    return InkWell(
+      onTap: () {
+        Get.to(const BookDetailsView());
+      },
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetPath.bookImage,
+              ),
             ),
           ),
         ),
