cc781dd0353c21f3a6e49535e1d5377c14c273ad
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-20 13:36:43 +0200

    refactor in CustomBookDetailsAppBar

--- lib/presentation_layer/view/book_details_view/custom_boo_details_app_bar.dart
+++ lib/presentation_layer/view/book_details_view/custom_boo_details_app_bar.dart
@@ -10,7 +10,7 @@ class CustomBookDetailsAppBar extends StatelessWidget {
       children: [
         IconButton(
           onPressed: () {
-            Navigator.pop;
+            Navigator.pop(context);
           },
           icon: const Icon(
             Icons.close,
