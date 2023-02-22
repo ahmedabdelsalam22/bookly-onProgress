0b0aedc43df9af321a8335365d0c89aa1ed3b3d2
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-22 14:36:33 +0200

    create SeeAllView

---
+++ lib/presentation_layer/screens/see_all_view/see_all_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/core/utilities/color_constants.dart';
+import 'package:bookly/core/utilities/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SeeAllView extends StatelessWidget {
+  const SeeAllView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorConstants.kPrimaryColor,
+      appBar: AppBar(
+        title: const Text(
+          'All Books',
+          style: AppTextStyles.textStyle18,
+        ),
+      ),
+    );
+  }
+}
