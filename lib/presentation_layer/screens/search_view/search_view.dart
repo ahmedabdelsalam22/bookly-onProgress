bc122ca8b95f8687f3314249a6631327ddcdbe7d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-22 13:42:16 +0200

    refactoring in search_view.dart

--- lib/presentation_layer/screens/search_view/search_view.dart
+++ lib/presentation_layer/screens/search_view/search_view.dart
@@ -1,14 +1,18 @@
 import 'package:bookly/presentation_layer/screens/search_view/search_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/utilities/color_constants.dart';
+
 class SearchView extends StatelessWidget {
   const SearchView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const SearchViewBody(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstants.kPrimaryColor,
+        body: const SearchViewBody(),
+      ),
     );
   }
 }
