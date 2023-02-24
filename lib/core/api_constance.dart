492e8e00aa3b49d8691135896b904beb1692bd07
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-24 13:51:59 +0200

    create movie_model.dart

---
+++ lib/core/api_constance.dart
@@ -0,0 +1,4 @@
+class ApiConstance {
+  static String baseUrl = "https://localhost:44337/";
+  static String getAllMovies = "${baseUrl}api/movies";
+}
---
+++ lib/data/model/movie_model.dart
@@ -0,0 +1,95 @@
+class Movie {
+  int? _id;
+  String? _title;
+  int? _year;
+  int? _rate;
+  String? _description;
+  String? _qoute;
+  String? _poster;
+  int? _genreId;
+  String? _genreName;
+
+  Movie(
+      {int? id,
+        String? title,
+        int? year,
+        int? rate,
+        String? description,
+        String? qoute,
+        String? poster,
+        int? genreId,
+        String? genreName}) {
+    if (id != null) {
+      this._id = id;
+    }
+    if (title != null) {
+      this._title = title;
+    }
+    if (year != null) {
+      this._year = year;
+    }
+    if (rate != null) {
+      this._rate = rate;
+    }
+    if (description != null) {
+      this._description = description;
+    }
+    if (qoute != null) {
+      this._qoute = qoute;
+    }
+    if (poster != null) {
+      this._poster = poster;
+    }
+    if (genreId != null) {
+      this._genreId = genreId;
+    }
+    if (genreName != null) {
+      this._genreName = genreName;
+    }
+  }
+
+  int? get id => _id;
+  set id(int? id) => _id = id;
+  String? get title => _title;
+  set title(String? title) => _title = title;
+  int? get year => _year;
+  set year(int? year) => _year = year;
+  int? get rate => _rate;
+  set rate(int? rate) => _rate = rate;
+  String? get description => _description;
+  set description(String? description) => _description = description;
+  String? get qoute => _qoute;
+  set qoute(String? qoute) => _qoute = qoute;
+  String? get poster => _poster;
+  set poster(String? poster) => _poster = poster;
+  int? get genreId => _genreId;
+  set genreId(int? genreId) => _genreId = genreId;
+  String? get genreName => _genreName;
+  set genreName(String? genreName) => _genreName = genreName;
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _title = json['title'];
+    _year = json['year'];
+    _rate = json['rate'];
+    _description = json['description'];
+    _qoute = json['qoute'];
+    _poster = json['poster'];
+    _genreId = json['genreId'];
+    _genreName = json['genreName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this._id;
+    data['title'] = this._title;
+    data['year'] = this._year;
+    data['rate'] = this._rate;
+    data['description'] = this._description;
+    data['qoute'] = this._qoute;
+    data['poster'] = this._poster;
+    data['genreId'] = this._genreId;
+    data['genreName'] = this._genreName;
+    return data;
+  }
+}
