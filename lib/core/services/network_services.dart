cbea54412b34da4e17020da0b4cbd74d2ee43258
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-24 13:38:20 +0200

    create NetworkServicesImpl

--- lib/core/services/network_services.dart
+++ lib/core/services/network_services.dart
@@ -1,3 +1,15 @@
+import 'package:dio/dio.dart';
+
 abstract class NetworkServices {
-  Future get(String url);
+  Future<Response<dynamic>> get(String url);
+}
+
+class NetworkServicesImpl implements NetworkServices {
+  final dio = Dio();
+
+  @override
+  Future<Response<dynamic>> get(String url) async {
+    var response = dio.get(url);
+    return response;
+  }
 }
