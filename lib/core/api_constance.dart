04f493ac8cb2e2d4a3c560bdcc225e0e252de022
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-28 17:54:56 +0200

    create new endPoints to get books by genreName

--- lib/core/api_constance.dart
+++ lib/core/api_constance.dart
@@ -2,8 +2,7 @@ class ApiEndPoints {
   static String baseUrl = "https://localhost:44374/api/";
   static String getAllBooks = "${baseUrl}books/getAllBooks";
   static String getAllGenres = "${baseUrl}genres/getAllGenres";
-
   static String getTopRated = "${baseUrl}books/getTopRated";
-
+  static String getBookByGenre(genreName) => "${baseUrl}books/$genreName";
   static String searchInBooks(String searchText) => "$getAllBooks/$searchText";
 }
